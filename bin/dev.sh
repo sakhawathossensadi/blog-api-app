7e9903d9074e91bc71d90523d41e0f544d9af975
sakhawathossensadi <newtonss919@gmail.com>
2022-08-26 11:16:41 +0600

    config bash file for local devlopment environment

---
+++ bin/dev.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+PROJECT_DIRECTORY=`pwd`
+source ./bin/_message-printer.sh
+# status "Swith to package directory"
+cd ..
+mkdir -p blog-packages ## Create directory if not exist
+cd blog-packages
+warning "Cloning halal packages into $PWD"
+if ([ ! -d "blog-api" ]) then
+    git clone https://github.com/sakhawathossensadi/blog-api.git
+    status 'blog-api package is cloned'
+fi
+status "Packages cloning is completed"
+# status "Switching to project directory $PROJECT_DIRECTORY"
+cd $PROJECT_DIRECTORY
+BRANCH_NAME="dev-local"
+git checkout $BRANCH_NAME
+composer update
